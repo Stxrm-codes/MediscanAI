14baea7a53279464c9244d08417ddd7f58dae717
Manthan <manthan2406mt@gmail.com>
2026-03-23 14:17:32 +0530

    updated some features

--- app.py
+++ app.py
@@ -231,5 +231,6 @@ def synthesize():
 
 
 # ─────────────────────────────────────────────────────────────────────────────
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+if _name_ == '_main_':
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
